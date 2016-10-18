4171de23d74ed1f49779ac1ad2a1c7f298b87d31
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2016-10-18 09:19:32 +0900

    fix command name

--- tools/record_ps_aux.sh
+++ tools/record_ps_aux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps aux | awk 'NR>1{print $0}' | el_stream hoge hoge-doc user=string pid=integer cpu=float mem=float vsz=long rss=long tty=non_analyzed stat=non_analyzed start=string time=string
+ps aux | awk 'NR>1{print $0}' | es_stream hoge hoge-doc user=string pid=integer cpu=float mem=float vsz=long rss=long tty=non_analyzed stat=non_analyzed start=string time=string
