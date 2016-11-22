56b088603780aec19da784827c6047d02c75bacd
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2016-11-22 10:34:27 +0900

    save log file

--- tools/record_link_state.sh
+++ tools/record_link_state.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 NOW=$(date --iso-8601=seconds)
-ip link show | grep mtu | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| es_stream link link-doc datetime=date  interface=non_analyzed state=non_analyzed 
+ip link show | grep mtu | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link link-doc datetime=date  interface=non_analyzed state=non_analyzed 
+for ns in $(ip netns)
+do
+    ip netns exec $ns ip link show | grep mtu | grep -v lo | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link link-doc datetime=date  interface=non_analyzed state=non_analyzed 
+done
--- tools/record_ps_aux.sh
+++ tools/record_ps_aux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NOW=$(date --iso-8601=seconds)
-ps aux | awk 'NR>1{print "'$NOW' " $0}' | es_stream process process-doc datetime=date user=string pid=integer cpu=float mem=float vsz=long rss=long tty=non_analyzed stat=non_analyzed start=string time=string
+ps aux | awk 'NR>1{print "'$NOW' " $0}' | tee -a process_stat.txt |  es_stream process process-doc datetime=date user=string pid=integer cpu=float mem=float vsz=long rss=long tty=non_analyzed stat=non_analyzed start=string time=string
