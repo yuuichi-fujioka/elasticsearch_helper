34337541afd774ddf5cae25e6eeca88304e3151c
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2017-04-21 08:37:58 +0900

    add date to index name

--- tools/record_link_state.sh
+++ tools/record_link_state.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 NOW=$(date --iso-8601=seconds)
-ip link show | grep mtu | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link link-doc datetime=date  interface=non_analyzed state=non_analyzed 
+DT=$(date --iso-8601)
+ip link show | grep mtu | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link-${DT} link-doc datetime=date  interface=non_analyzed state=non_analyzed 
 for ns in $(ip netns)
 do
-    ip netns exec $ns ip link show | grep mtu | grep -v lo | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link link-doc datetime=date  interface=non_analyzed state=non_analyzed 
+    ip netns exec $ns ip link show | grep mtu | grep -v lo | sed "s/^[^ ]\+ \+//g" | sed "s/:.*state / /g" | sed "s/ mode.*//g" | awk '{print "'$NOW' " $0}'| tee -a linkstate.txt | es_stream link-${DT} link-doc datetime=date  interface=non_analyzed state=non_analyzed 
 done
