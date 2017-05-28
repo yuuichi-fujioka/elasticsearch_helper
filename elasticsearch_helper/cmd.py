ac41a5723302dbb29696379691b4638277f02929
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2017-05-28 23:52:42 +0900

    fix delta value to average

--- elasticsearch_helper/cmd.py
+++ elasticsearch_helper/cmd.py
@@ -24,48 +24,63 @@ class Mapping(object):
             # delta
             if type in ['integer', 'long', 'float', 'double']:
                 if type == 'integer':
-                    convreter = int
+                    converter = int
                 if type == 'long':
-                    convreter = long
+                    converter = long
                 if type == 'float':
-                    convreter = float
+                    converter = float
                 if type == 'double':
-                    convreter = float
-                return lambda v, p: (
-                    convreter(v) - (convreter(p) if p is not None else convreter(v))) if v else v  # noqa
+                    converter = float
+
+                def _(v, p, delta):
+                    if p is None:
+                        p = v
+                    delta_time = delta.total_seconds()
+                    if delta_time != 0:
+                        return (
+                            (converter(v) - converter(p)) / delta_time)
+                    else:
+                        return (
+                            (converter(v) - converter(p)))
+                return _
             if type == 'date':
-                return lambda v, p: (parser.parse(v) - (parser.parse(p) if p is not None else parser.parse(v))).total_seconds()  # noqa
-            return lambda v, p: v
+                return lambda v, p, delta: (parser.parse(v) - (parser.parse(p) if p is not None else parser.parse(v))).total_seconds()  # noqa
+            return lambda v, p, delta: v
         else:
             # direct
             if type == 'integer':
-                return lambda v, p: int(v)
+                return lambda v, p, delta: int(v)
             if type == 'long':
-                return lambda v, p: long(v)
+                return lambda v, p, delta: long(v)
             if type == 'float':
-                return lambda v, p: float(v)
+                return lambda v, p, delta: float(v)
             if type == 'double':
-                return lambda v, p: float(v)
+                return lambda v, p, delta: float(v)
             if type == 'date':
-                return lambda v, p: parser.parse(v) if v else v
-            return lambda v, p: v
+                return lambda v, p, delta: parser.parse(v) if v else v
+            return lambda v, p, delta: v
 
 
 def _parse_mapping_args(argv):
     mappings = []
+    datetime_index = -1
+    index = 0
     for kv in argv:
         if kv != 'skip':
             key, _, type = kv.partition('=')
             mappings.append(Mapping(key, type))
         else:
             mappings.append(Mapping('', 'skip'))
-    return mappings
+            if datetime_index < 0:
+                datetime_index = index
+        index += 1
+    return mappings, datetime_index
 
 
 def stream():
     index_name = sys.argv[1]
     doc_type = sys.argv[2]
-    mappings = _parse_mapping_args(sys.argv[3:])
+    mappings, datetime_index = _parse_mapping_args(sys.argv[3:])
 
     types = set([m.type for m in mappings if m != 'skip'])
 
@@ -76,6 +91,7 @@ def stream():
     index_mapping['host'] = 'non_analyzed'
     if 'date' not in types:
         index_mapping['@time'] = 'date'
+
     index_helper.make_index(
         es, index_name, doc_type, **index_mapping)
 
@@ -84,16 +100,26 @@ def stream():
     with bulk_helper.bulk(es) as putter:
         try:
             prev_values = []
+            prev_date = None
             while True:
                 line = raw_input()
                 values = line.split(None, parameter_len)
+                if datetime_index >= 0:
+                    now = parser.parse(values[datetime_index])
+                else:
+                    now = datetime.datetime.utcnow()
+                if prev_date is not None:
+                    delta = now - prev_date
+                else:
+                    delta = datetime.timedelta()
                 data = {
-                    m.key if m and m.key else "data": m.parse(v, p) if m else v
+                    m.key if m and m.key else "data":
+                    m.parse(v, p, delta) if m else v
                     for m, v, p in itertools.izip_longest(
                         mappings, values, prev_values)
                     if not m or m.type != "skip"}
                 if 'date' not in types:
-                    data['@time'] = datetime.datetime.utcnow()
+                    data['@time'] = now
                 data['host'] = hostname
                 putter(index_name, None, doc_type, data)
                 prev_values = values
